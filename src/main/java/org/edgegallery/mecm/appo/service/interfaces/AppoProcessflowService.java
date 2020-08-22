757ba1626940121e5658cdeb3c9b3bf55e2714fb
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2020-08-22 23:37:13 +0530

    param name change

--- src/main/java/org/edgegallery/mecm/appo/service/interfaces/AppoProcessflowService.java
+++ src/main/java/org/edgegallery/mecm/appo/service/interfaces/AppoProcessflowService.java
@@ -8,17 +8,17 @@ public interface AppoProcessflowService {
     /**
      * Start processing process flow asychronously.
      *
-     * @param processName  process key
+     * @param processKey   process key
      * @param requestInput input parameters
      */
-    void executeProcessAsync(String processName, Map<String, String> requestInput);
+    void executeProcessAsync(String processKey, Map<String, String> requestInput);
 
     /**
      * Start processing process flow sychronously.
      *
-     * @param processName  process key
+     * @param processKey   process key
      * @param requestInput input parameters
      * @return workflow response
      */
-    AppoProcessFlowResponse executeProcessSync(String processName, Map<String, String> requestInput);
+    AppoProcessFlowResponse executeProcessSync(String processKey, Map<String, String> requestInput);
 }
