01d01f4b9916306eee3903b1b31b482cf91b270a
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2020-08-28 07:39:47 +0530

    app instance info repository

---
+++ src/main/java/org/edgegallery/mecm/appo/repository/AppInstanceInfoRepository.java
@@ -0,0 +1,8 @@
+package org.edgegallery.mecm.appo.repository;
+
+import org.edgegallery.mecm.appo.model.AppInstanceInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppInstanceInfoRepository extends CrudRepository<AppInstanceInfo, String> {
+
+}
