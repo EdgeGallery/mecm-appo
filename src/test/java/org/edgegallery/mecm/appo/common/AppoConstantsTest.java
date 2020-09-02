57924bb50b11006c6e32b966ac0742f76f2a997a
Pramod <pramod.kumar6@huawei.com>
2020-09-02 14:24:16 +0530

    Constant class for test case added

---
+++ src/test/java/org/edgegallery/mecm/appo/common/AppoConstantsTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.edgegallery.mecm.appo.common;
+
+public class AppoConstantsTest {
+
+    public static final String Access_Token = "access_token";
+
+    public static final String App_Descriptor = "app_descriptor";
+
+    public static final String APP_ID = "AppId";
+
+    public static final String App_Instance_Id = "app_instance_id";
+
+    public static final String App_Package_Id = "app_package_id";
+
+    public static final String Action = "action";
+
+    public static final String Applcm_Ip = "applcm_ip";
+
+    public static final String Applcm_LCM_HOST = "applcm_lcm_host";
+
+    public static final String Applcm_Port = "applcm_port";
+
+    public static final String App_Instance_Descripton = "app_instance_description";
+
+    public static final String App_Name = "app_name";
+
+    public static final String Download = "download";
+
+    public static final String HOST_IP = "host_ip";
+
+    public static final String Mec_Host = "mec_host";
+
+    public static final String Message = "message";
+
+    public static final String Operation_Type = "operationType";
+
+    public static final String Request_Action = "requestAction";
+
+    public static final String Response_Type = "responseType";
+
+    public static final String Response = "Response";
+
+    public static final String Response_Code = "ResponseCode";
+
+    public static final String Tenant = "tenant";
+
+    public static final String Tenant_Id = "tenantId";
+
+    public static final String Operational_Status = "operationalStatus";
+
+    public static final String Operational_Info = "operationalInfo";
+}
