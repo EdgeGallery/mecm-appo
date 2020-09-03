a23c12a4ba88e1d6d0569373fe1c073629c0a897
root1 <vidyashree.rama@huawei.com>
2020-09-03 14:29:19 +0530

    adding max json request size limit

---
+++ src/main/java/org/edgegallery/mecm/appo/apihandler/filter/JsonRequestSizeLimitFilter.java
@@ -0,0 +1,60 @@
+/*
+ *    Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.edgegallery.mecm.appo.apihandler.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+public class JsonRequestSizeLimitFilter extends OncePerRequestFilter {
+
+    private static final long MAX_JSON_POST_SIZE = 10485760; // 10MB
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (isApplicationJson(request) && request.getContentLengthLong() > MAX_JSON_POST_SIZE) {
+            resolver.resolveException(request, response, null,
+                    new IllegalArgumentException("request content exceeded limit of 10MB"));
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isApplicationJson(HttpServletRequest httpRequest) {
+        if (httpRequest.getHeader(HttpHeaders.CONTENT_TYPE) != null) {
+            return MediaType.APPLICATION_JSON.isCompatibleWith(MediaType
+                    .parseMediaType(httpRequest.getHeader(HttpHeaders.CONTENT_TYPE)));
+        }
+        return false;
+    }
+}
