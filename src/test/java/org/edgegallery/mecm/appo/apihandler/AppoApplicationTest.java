e63376bae0521381b7d3ffeaa07a521a95510188
agrawalgaurav <gaurav.agrawal@huawei.com>
2020-09-06 13:09:18 +0530

    Added framework for flow based UT

---
+++ src/test/java/org/edgegallery/mecm/appo/apihandler/AppoApplicationTest.java
@@ -0,0 +1,49 @@
+/*
+ *    Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.edgegallery.mecm.appo.apihandler;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@SpringBootApplication(scanBasePackages = "org.edgegallery.mecm.appo", exclude = {SecurityAutoConfiguration.class})
+@EnableScheduling
+public class AppoApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppoApplicationTest.class, args);
+    }
+
+    @Bean
+    public OncePerRequestFilter accessTokenFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain filterChain) throws ServletException, IOException {
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+}
