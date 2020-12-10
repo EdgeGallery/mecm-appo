7b1a23d5e2baa6941f6e1ce86dc105f8acbb8a29
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2020-12-10 18:29:37 +0530

    max tenant count increased to 20

--- src/main/java/org/edgegallery/mecm/appo/utils/Constants.java
+++ src/main/java/org/edgegallery/mecm/appo/utils/Constants.java
@@ -103,7 +103,7 @@ public final class Constants {
     public static final String SUCCESS = "success";
 
     public static final int MAX_ENTRY_PER_TENANT_PER_MODEL = 50;
-    public static final int MAX_TENANTS = 10;
+    public static final int MAX_TENANTS = 20;
     public static final String MAX_LIMIT_REACHED_ERROR = "Max record limit exceeded";
 
     public static final String SLASH = "/";
