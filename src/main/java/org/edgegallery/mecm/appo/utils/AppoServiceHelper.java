2d046de1ef4028c47a7955732529a420f4166fc0
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2021-06-08 18:41:11 +0530

    vm based app deploy support

---
+++ src/main/java/org/edgegallery/mecm/appo/utils/AppoServiceHelper.java
@@ -0,0 +1,112 @@
+/*
+ *  Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.edgegallery.mecm.appo.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.edgegallery.mecm.appo.exception.AppoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class AppoServiceHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppoServiceHelper.class);
+
+    static final int TOO_MANY = 1024;
+    static final int TOO_BIG = 104857600;
+
+    private AppoServiceHelper() {
+    }
+
+    static String sanitizeFileName(String entryName, String intendedDir) throws IOException {
+        File f = new File(intendedDir, entryName);
+        String canonicalPath = f.getCanonicalPath();
+        File intendDir = new File(intendedDir);
+        if (intendDir.isDirectory() && !intendDir.exists()) {
+            createFile(intendedDir);
+        }
+        String canonicalID = intendDir.getCanonicalPath();
+        if (canonicalPath.startsWith(canonicalID)) {
+            return canonicalPath;
+        } else {
+            throw new IllegalStateException("file is outside extraction target directory.");
+        }
+    }
+
+    static void createFile(String filePath) throws IOException {
+        File tempFile = new File(filePath);
+        boolean result = false;
+
+        if (!tempFile.getParentFile().exists() && !tempFile.isDirectory()) {
+            result = tempFile.getParentFile().mkdirs();
+        }
+        if (!tempFile.exists() && !tempFile.isDirectory() && !tempFile.createNewFile() && !result) {
+            throw new IllegalArgumentException("create temp file failed");
+        }
+    }
+
+    public static boolean isFileWithSuffixExist(String str, String suffix) {
+        return StringUtils.endsWith(str, suffix);
+    }
+
+    /**
+     * Returns software image descriptor content in string format.
+     *
+     * @param localFilePath CSAR file path
+     * @param intendedDir   intended directory
+     */
+    public static void unzipApplicationPacakge(String localFilePath, String intendedDir) {
+
+        LOGGER.debug("unzip package....");
+        try (ZipFile zipFile = new ZipFile(localFilePath)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            int entriesCount = 0;
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                if (entriesCount > TOO_MANY) {
+                    throw new IllegalStateException("too many files to unzip");
+                }
+                entriesCount++;
+                // sanitize file path
+                String fileName = sanitizeFileName(entry.getName(), intendedDir);
+                if (!entry.isDirectory()) {
+                    try (InputStream inputStream = zipFile.getInputStream(entry)) {
+                        if (inputStream.available() > TOO_BIG) {
+                            throw new IllegalStateException("file being unzipped is too big");
+                        }
+                        FileUtils.copyInputStreamToFile(inputStream, new File(fileName));
+                        LOGGER.info("unzip package... {}", entry.getName());
+                    }
+                } else {
+
+                    File dir = new File(fileName);
+                    boolean dirStatus = dir.mkdirs();
+                    LOGGER.debug("creating dir {}, status {}", fileName, dirStatus);
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error(Constants.FAILED_TO_UNZIP_CSAR);
+            throw new AppoException(Constants.FAILED_TO_UNZIP_CSAR);
+        }
+    }
+}
